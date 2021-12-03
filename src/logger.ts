67703375106aafb192f02caf61c3997c92b27dbd
cicerotcv <elm.tiago@gmail.com>
2021-12-03 17:45:52 -0300

    create log file if no exists

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,11 @@
 import logger from "morgan";
 import fs from "fs";
 
-const LOGGER_FOLDER_NAME = "logs";
+// const LOGGER_FOLDER_NAME = "logs";
 const LOGGER_FILE_NAME = "access.log";
 
 const accessLogStream = fs.createWriteStream(
-  `${__dirname}/${LOGGER_FOLDER_NAME}/${LOGGER_FILE_NAME}`,
+  `${__dirname}/${LOGGER_FILE_NAME}`,
   { flags: "a" }
 );
 export const consoleLogger = logger("common");
